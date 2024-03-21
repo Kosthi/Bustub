0dfed58001d4aa1eea63dbd01d11e3f5b67e73f0
Koschei <nitianzero@gmail.com>
2024-03-21 19:34:36 +0800

    feat: implement LRU-K replacer

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -17,14 +17,91 @@ namespace bustub {
 
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::lock_guard<std::mutex> lock(latch_);
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
+  frame_id_t fid1;
+  frame_id_t fid2;
+  size_t min_time_stamp1 = 0xffffff;
+  size_t min_time_stamp2 = 0xffffff;
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+  for (auto &it : node_store_) {
+    auto &node = it.second;
+    if (node.is_evictable_) {
+      if (node.history_.size() < k_ && node.history_.front() < min_time_stamp1) {
+        min_time_stamp1 = node.history_.front();
+        fid1 = it.first;
+      } else if (node.history_.size() == k_ && node.history_.front() < min_time_stamp2) {
+        min_time_stamp2 = node.history_.front();
+        fid2 = it.first;
+      }
+    }
+  }
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+  if (min_time_stamp1 != 0xffffff) {
+    --curr_size_;
+    node_store_.erase(fid1);
+    *frame_id = fid1;
+    return true;
+  }
+  if (min_time_stamp2 != 0xffffff) {
+    --curr_size_;
+    node_store_.erase(fid2);
+    *frame_id = fid2;
+    return true;
+  }
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+  return false;
+}
+
+void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
+  std::lock_guard<std::mutex> lock(latch_);
+
+  if (static_cast<size_t>(frame_id) >= replacer_size_) {
+    throw ExecutionException("[LRUKReplacer] The frame id is invalid when calling RecordAccess function.");
+  }
+
+  auto it = node_store_.find(frame_id);
+  if (it == node_store_.end()) {
+    it = node_store_.emplace(frame_id, LRUKNode()).first;
+  }
+  if (it->second.history_.size() == k_) {
+    it->second.history_.pop_back();
+  }
+  it->second.history_.emplace_front(current_timestamp_++);
+}
+
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  std::lock_guard<std::mutex> lock(latch_);
+
+  auto it = node_store_.find(frame_id);
+  if (it == node_store_.end()) {
+    throw ExecutionException("[LRUKReplacer] The frame id is invalid when calling SetEvictable function.");
+  }
+  if (it->second.is_evictable_ && !set_evictable) {
+    --curr_size_;
+  } else if (!it->second.is_evictable_ && set_evictable) {
+    ++curr_size_;
+  }
+  it->second.is_evictable_ = set_evictable;
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  std::lock_guard<std::mutex> lock(latch_);
+
+  auto it = node_store_.find(frame_id);
+  if (it == node_store_.end()) {
+    return;
+  }
+
+  if (!it->second.is_evictable_) {
+    throw ExecutionException("[LRUKReplacer] Remove is called on a non-evictable frame when calling Remove function.");
+  }
+
+  --curr_size_;
+  node_store_.erase(it);
+}
+
+auto LRUKReplacer::Size() -> size_t { return curr_size_; }
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -25,17 +25,6 @@ namespace bustub {
 
 enum class AccessType { Unknown = 0, Lookup, Scan, Index };
 
-class LRUKNode {
- private:
-  /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
-  // Remove maybe_unused if you start using them. Feel free to change the member variables as you want.
-
-  [[maybe_unused]] std::list<size_t> history_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] frame_id_t fid_;
-  [[maybe_unused]] bool is_evictable_{false};
-};
-
 /**
  * LRUKReplacer implements the LRU-k replacement policy.
  *
@@ -49,6 +38,19 @@ class LRUKNode {
  */
 class LRUKReplacer {
  public:
+  class LRUKNode {
+    friend class LRUKReplacer;
+
+   private:
+    /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
+    // Remove maybe_unused if you start using them. Feel free to change the member variables as you want.
+
+    std::list<size_t> history_;
+    [[maybe_unused]] size_t k_;
+    [[maybe_unused]] frame_id_t fid_;
+    bool is_evictable_{false};
+  };
+
   /**
    *
    * TODO(P1): Add implementation
@@ -150,12 +152,12 @@ class LRUKReplacer {
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_store_;
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] std::mutex latch_;
+  std::unordered_map<frame_id_t, LRUKNode> node_store_;
+  size_t current_timestamp_{0};
+  std::atomic<size_t> curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
+  std::mutex latch_;
 };
 
 }  // namespace bustub
