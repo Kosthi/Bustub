e6eb590a3f2b83cc7a340498ed00c2d1ccff9c29
Koschei <nitianzero@gmail.com>
2024-04-05 15:45:16 +0800

    test: enhance tests with RecursiveMergeTest

--- test/container/disk/hash/extendible_htable_test.cpp
+++ test/container/disk/hash/extendible_htable_test.cpp
@@ -10,6 +10,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <random>
 #include <thread>  // NOLINT
 #include <vector>
 
@@ -189,10 +190,118 @@ TEST(ExtendibleHTableTest, RemoveTest2) {
 }
 
 // NOLINTNEXTLINE
-TEST(ExtendibleHTableTest, GrowShrinkTest) {
+TEST(ExtendibleHTableTest, RemoveTest3) {
   auto disk_mgr = std::make_unique<DiskManagerUnlimitedMemory>();
   auto bpm = std::make_unique<BufferPoolManager>(50, disk_mgr.get());
+  DiskExtendibleHashTable<int, int, IntComparator> ht("blah", bpm.get(), IntComparator(), HashFunction<int>(), 0, 3, 3);
+  ht.VerifyIntegrity();
 
+  std::vector<int> insert_num{28, 8, 25, 30, 22, 18, 11, 27, 23, 7};
+  // insert some values
+  for (int i : insert_num) {
+    bool inserted = ht.Insert(i, i);
+    ASSERT_TRUE(inserted);
+    std::vector<int> res;
+    ht.GetValue(i, &res);
+    ASSERT_EQ(1, res.size());
+    ASSERT_EQ(i, res[0]);
+  }
+
+  ht.VerifyIntegrity();
+
+  ht.PrintHT();
+
+  // 创建随机数引擎
+  std::random_device rd;
+  std::mt19937 g(rd());
+  // 打乱向量
+  std::shuffle(insert_num.begin(), insert_num.end(), g);
+
+  auto remove_num = insert_num;
+  // std::vector<int> remove_num{18, 23, 25, 22, 28, 8, 7};
+  for (int i : remove_num) {
+    std::cout << "afert removing " << i << '\n';
+    bool removed = ht.Remove(i);
+    ASSERT_TRUE(removed);
+    std::vector<int> res;
+    ht.GetValue(i, &res);
+    ASSERT_EQ(0, res.size());
+    ht.PrintHT();
+  }
+}
+
+// NOLINTNEXTLINE
+TEST(ExtendibleHTableTest, GrowShrinkTest1) {
+  auto disk_mgr = std::make_unique<DiskManagerUnlimitedMemory>();
+  auto bpm = std::make_unique<BufferPoolManager>(3, disk_mgr.get());
+
+  DiskExtendibleHashTable<int, int, IntComparator> ht("blah", bpm.get(), IntComparator(), HashFunction<int>(), 9, 9,
+                                                      511);
+
+  // 创建一个存有0-9的向量
+  std::vector<int> nums;
+
+  // 创建随机数引擎
+  std::random_device rd;
+  std::mt19937 g(rd());
+
+  // insert some values
+  for (int i = 0; i < 1000; i++) {
+    nums.emplace_back(i);
+    bool inserted = ht.Insert(i, i);
+    ASSERT_TRUE(inserted);
+    std::vector<int> res;
+    ht.GetValue(i, &res);
+    ASSERT_EQ(1, res.size());
+    ASSERT_EQ(i, res[0]);
+  }
+
+  // 打乱向量
+  std::shuffle(nums.begin(), nums.end(), g);
+
+  ht.VerifyIntegrity();
+
+  for (auto i : nums) {
+    bool inserted = ht.Remove(i);
+    ASSERT_TRUE(inserted);
+    std::vector<int> res;
+    ht.GetValue(i, &res);
+    ASSERT_EQ(0, res.size());
+  }
+
+  ht.VerifyIntegrity();
+
+  nums.clear();
+
+  for (int i = 1000; i < 2000; i++) {
+    nums.emplace_back(i);
+    bool inserted = ht.Insert(i, i);
+    ASSERT_TRUE(inserted);
+    std::vector<int> res;
+    ht.GetValue(i, &res);
+    ASSERT_EQ(1, res.size());
+    ASSERT_EQ(i, res[0]);
+  }
+
+  std::shuffle(nums.begin(), nums.end(), g);
+
+  ht.VerifyIntegrity();
+
+  for (auto &i : nums) {
+    bool inserted = ht.Remove(i);
+    ASSERT_TRUE(inserted);
+    std::vector<int> res;
+    ht.GetValue(i, &res);
+    ASSERT_EQ(0, res.size());
+  }
+
+  ht.VerifyIntegrity();
+}
+
+// NOLINTNEXTLINE
+TEST(ExtendibleHTableTest, GrowShrinkTest2) {
+  auto disk_mgr = std::make_unique<DiskManagerUnlimitedMemory>();
+  auto bpm = std::make_unique<BufferPoolManager>(3, disk_mgr.get());
   DiskExtendibleHashTable<int, int, IntComparator> ht("blah", bpm.get(), IntComparator(), HashFunction<int>(), 9, 9,
                                                       511);
   // insert some values
@@ -243,13 +352,39 @@ TEST(ExtendibleHTableTest, GrowShrinkTest) {
     ht.GetValue(i, &res);
     ASSERT_EQ(0, res.size());
   }
+  ht.VerifyIntegrity();
+}
 
-  for (int i = 1500; i < 2000; i++) {
-    bool inserted = ht.Remove(i);
+// NOLINTNEXTLINE
+TEST(ExtendibleHTableTest, RecursiveMergeTest) {
+  auto disk_mgr = std::make_unique<DiskManagerUnlimitedMemory>();
+  auto bpm = std::make_unique<BufferPoolManager>(50, disk_mgr.get());
+  DiskExtendibleHashTable<int, int, IntComparator> ht("blah", bpm.get(), IntComparator(), HashFunction<int>(), 0, 3, 2);
+  ht.VerifyIntegrity();
+
+  int insert_num[]{4, 5, 6, 14};
+  // insert some values
+  for (int i : insert_num) {
+    bool inserted = ht.Insert(i, i);
     ASSERT_TRUE(inserted);
     std::vector<int> res;
     ht.GetValue(i, &res);
+    ASSERT_EQ(1, res.size());
+    ASSERT_EQ(i, res[0]);
+  }
+
+  ht.VerifyIntegrity();
+
+  ht.PrintHT();
+
+  int remove_num[]{5, 14, 4};
+  for (int i : remove_num) {
+    bool removed = ht.Remove(i);
+    ASSERT_TRUE(removed);
+    std::vector<int> res;
+    ht.GetValue(i, &res);
     ASSERT_EQ(0, res.size());
+    ht.PrintHT();
   }
 
   ht.VerifyIntegrity();
