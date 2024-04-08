5172869b1794f95806981b88e8687bc6450efe4d
Koschei <nitianzero@gmail.com>
2024-04-08 17:46:09 +0800

    feat: implement seq_scan_executor

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,31 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx), plan_(plan) {
+  auto &&catalog = exec_ctx->GetCatalog();
+  auto &&table_info = catalog->GetTable(plan_->table_oid_);
+  table_iterator_ = std::make_unique<TableIterator>(table_info->table_->MakeIterator());
+}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  if (table_iterator_->IsEnd()) {
+    return false;
+  }
+  auto &it = *table_iterator_;
+  while (!it.IsEnd()) {
+    auto &&[meta, to_tuple] = it.GetTuple();
+    if (!meta.is_deleted_) {
+      *tuple = std::move(to_tuple);
+      *rid = it.GetRID();
+      ++it;
+      return true;
+    }
+    ++it;
+  }
+  return false;
+}
 
 }  // namespace bustub
