8dd2f2d755722fbed20ad31f498330dbe2144069
Koschei <nitianzero@gmail.com>
2024-04-12 16:49:31 +0800

    feat: implement insert_executor.h

--- src/include/execution/executors/insert_executor.h
+++ src/include/execution/executors/insert_executor.h
@@ -57,6 +57,9 @@ class InsertExecutor : public AbstractExecutor {
  private:
   /** The insert plan node to be executed*/
   const InsertPlanNode *plan_;
+  std::unique_ptr<AbstractExecutor> child_executor_;
+  TableHeap *table_heap_;
+  int inserted_{0};
 };
 
 }  // namespace bustub
