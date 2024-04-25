1d0ef41324e84dccdad2ff036a7a57cbcf3bf4ae
Koschei <nitianzero@gmail.com>
2024-04-25 19:43:16 +0800

    feat: implement window functions

--- src/execution/window_function_executor.cpp
+++ src/execution/window_function_executor.cpp
@@ -1,14 +1,177 @@
 #include "execution/executors/window_function_executor.h"
+#include "execution/executors/seq_scan_executor.h"
+#include "execution/expressions/column_value_expression.h"
 #include "execution/plans/window_plan.h"
 #include "storage/table/tuple.h"
 
 namespace bustub {
 
+// 假设每个窗口函数的partition by都一致，则通过mysql知，都是根据partition by大小升序排列输出
 WindowFunctionExecutor::WindowFunctionExecutor(ExecutorContext *exec_ctx, const WindowFunctionPlanNode *plan,
                                                std::unique_ptr<AbstractExecutor> &&child_executor)
     : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void WindowFunctionExecutor::Init() { throw NotImplementedException("WindowFunctionExecutor is not implemented"); }
+void WindowFunctionExecutor::Init() {
+  origin_tuples_.clear();
+  sort_tuples_.clear();
+  ahts_.clear();
+  aht_iterators_.clear();
+  child_executor_->Init();
+  row_idx_ = 0;
+  cnt_table_.clear();
+  sort_idx_ = 0;
 
-auto WindowFunctionExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+  while (child_executor_->Next(&tuple_, &rid_)) {
+    origin_tuples_.emplace_back(tuple_);
+  }
+
+  for (std::size_t i = 0; i < plan_->columns_.size(); ++i) {
+    if (const auto *col_expr = dynamic_cast<const ColumnValueExpression *>(plan_->columns_[i].get());
+        col_expr != nullptr) {
+      // 行值表达式
+      if (col_expr->GetColIdx() != UINT32_MAX) {
+        sort_tuples_.emplace_back(origin_tuples_);
+        ahts_.emplace_back(std::vector<AbstractExpressionRef>{}, std::vector<WindowFunctionType>{});
+        cnt_table_.emplace_back();
+      } else {
+        auto &&window_function = plan_->window_functions_.at(i);
+        ahts_.emplace_back(std::vector<AbstractExpressionRef>{window_function.function_},
+                           std::vector<WindowFunctionType>{window_function.type_});
+        if (!window_function.order_by_.empty()) {
+          sort_idx_ = i;
+        } else {
+          if (!window_function.partition_by_.empty()) {
+            sort_idx_ = i;
+          }
+          // 没有sort 照原始顺序聚合但partition要升序
+          for (auto &tuple : origin_tuples_) {
+            ahts_.back().InsertCombine(MakeAggregateKey(&tuple, window_function.partition_by_),
+                                       MakeAggregateValue(&tuple, window_function.function_));
+          }
+        }
+
+        // 无论是part还是sort都要sort
+        // C++20 support structured bindings.
+        sort_tuples_.emplace_back(origin_tuples_);
+        if (!window_function.order_by_.empty() || !window_function.partition_by_.empty()) {
+          std::sort(sort_tuples_.back().begin(), sort_tuples_.back().end(), [&](const Tuple &a, const Tuple &b) {
+            // 如果有partition则要在窗口内排序
+            if (!window_function.partition_by_.empty()) {
+              for (auto &expr : window_function.partition_by_) {
+                auto &&lhs = expr->Evaluate(&a, *plan_->output_schema_);
+                auto &&rhs = expr->Evaluate(&b, *plan_->output_schema_);
+                if (lhs.CompareEquals(rhs) != CmpBool::CmpTrue) {
+                  return lhs.CompareLessThan(rhs) == CmpBool::CmpTrue;
+                }
+              }
+            }
+
+            for (auto &[order_type, expr] : window_function.order_by_) {
+              auto &&lhs = expr->Evaluate(&a, *plan_->output_schema_);
+              auto &&rhs = expr->Evaluate(&b, *plan_->output_schema_);
+              if (order_type == OrderByType::DEFAULT || order_type == OrderByType::ASC) {
+                if (lhs.CompareEquals(rhs) != CmpBool::CmpTrue) {
+                  return lhs.CompareLessThan(rhs) == CmpBool::CmpTrue;
+                }
+              } else if (order_type == OrderByType::DESC) {
+                if (lhs.CompareEquals(rhs) != CmpBool::CmpTrue) {
+                  return lhs.CompareGreaterThan(rhs) == CmpBool::CmpTrue;
+                }
+              } else {
+                BUSTUB_ASSERT(false, "Error OrderByType.");
+              }
+            }
+            return false;
+          });
+        }
+
+        cnt_table_.emplace_back();
+        for (auto &tuple : origin_tuples_) {
+          auto &&itt = cnt_table_.back().find(MakeAggregateKey(&tuple, window_function.partition_by_));
+          if (itt != cnt_table_.back().end()) {
+            ++itt->second;
+          } else {
+            cnt_table_.back().emplace(MakeAggregateKey(&tuple, window_function.partition_by_), 1);
+          }
+        }
+        // wrong use
+        // aht_iterators_.emplace_back(ahts_.back().Begin());
+      }
+    } else {
+      BUSTUB_ASSERT(false, "Error Expression Type.");
+    }
+  }
+
+  for (auto &aht : ahts_) {
+    aht_iterators_.emplace_back(aht.Begin());
+  }
+}
+
+auto WindowFunctionExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  if (row_idx_ == origin_tuples_.size()) {
+    return false;
+  }
+
+  std::vector<Value> values;
+  values.reserve(plan_->columns_.size());
+
+  for (std::size_t i = 0; i < plan_->columns_.size(); ++i) {
+    auto &&expr = plan_->columns_[i];
+    if (const auto *col_expr = dynamic_cast<const ColumnValueExpression *>(expr.get()); expr != nullptr) {
+      // 列值表达式
+      if (col_expr->GetColIdx() != UINT32_MAX) {
+        *tuple = sort_tuples_[sort_idx_][row_idx_];
+        values.emplace_back(col_expr->Evaluate(tuple, plan_->OutputSchema()));
+        continue;
+      }
+      auto &&window_function = plan_->window_functions_.at(i);
+      // 聚合表达式
+      if (window_function.order_by_.empty()) {
+        // 处理所有行的聚合
+        // 实际上就一个聚合函数
+        for (auto &v : aht_iterators_[i].Val().aggregates_) {
+          values.emplace_back(v);
+        }
+        if (!aht_iterators_[i].Key().group_bys_.empty()) {
+          if (--cnt_table_[i][aht_iterators_[i].Key()] == 0) {
+            ++aht_iterators_[i];
+          }
+        }
+      } else {
+        auto &&aggregate_key =
+            MakeAggregateKey(&sort_tuples_[i][row_idx_], plan_->window_functions_.at(i).partition_by_);
+        auto &&aggregate_value =
+            MakeAggregateValue(&sort_tuples_[i][row_idx_], plan_->window_functions_.at(i).function_);
+        // 对于rank()特殊处理
+        if (plan_->window_functions_.at(i).type_ == WindowFunctionType::Rank && row_idx_ > 0) {
+          auto &&prev = sort_tuples_[i][row_idx_ - 1].GetValue(plan_->output_schema_.get(), 0);
+          auto &&curr = sort_tuples_[i][row_idx_].GetValue(plan_->output_schema_.get(), 0);
+          if (prev.CompareEquals(curr) != CmpBool::CmpTrue) {
+            while ((cnt_duplication_--) != 0U) {
+              ahts_[i].InsertCombine(aggregate_key, aggregate_value);
+            }
+            cnt_duplication_ = 1;
+          } else {
+            ++cnt_duplication_;
+          }
+        } else {
+          // 处理第一个到当前行的聚合
+          ahts_[i].InsertCombine(aggregate_key, aggregate_value);
+        }
+        // 插入新数据以后原来的迭代器会失效！！！
+        auto &&it = ahts_[i].Begin();
+        for (auto &v : it.Val().aggregates_) {
+          values.emplace_back(v);
+        }
+        if (--cnt_table_[i][it.Key()] == 0) {
+          ahts_[i].Erase(it.Key());
+        }
+      }
+    }
+  }
+
+  ++row_idx_;
+  *tuple = Tuple(values, plan_->output_schema_.get());
+  return true;
+}
 }  // namespace bustub
--- src/include/execution/executors/window_function_executor.h
+++ src/include/execution/executors/window_function_executor.h
@@ -17,6 +17,8 @@
 
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
+#include "execution/executors/aggregation_executor.h"
+#include "execution/executors/sort_executor.h"
 #include "execution/plans/window_plan.h"
 #include "storage/table/tuple.h"
 
@@ -85,10 +87,209 @@ class WindowFunctionExecutor : public AbstractExecutor {
   auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); }
 
  private:
+  /**
+   * A simplified hash table that has all the necessary functionality for aggregations.
+   */
+  class AggregationHashTable {
+   public:
+    /**
+     * Construct a new SimpleAggregationHashTable instance.
+     * @param agg_exprs the aggregation expressions
+     * @param agg_types the types of aggregations
+     */
+    AggregationHashTable(std::vector<AbstractExpressionRef> agg_exprs, std::vector<WindowFunctionType> agg_types)
+        : agg_exprs_(std::move(agg_exprs)), agg_types_(std::move(agg_types)) {}
+
+    /** @return The initial aggregate value for this aggregation executor */
+    auto GenerateInitialAggregateValue() -> AggregateValue {
+      std::vector<Value> values{};
+      for (const auto &agg_type : agg_types_) {
+        switch (agg_type) {
+          case WindowFunctionType::CountStarAggregate:
+            // Count start starts at zero.
+            values.emplace_back(ValueFactory::GetIntegerValue(0));
+            break;
+          case WindowFunctionType::CountAggregate:
+          case WindowFunctionType::SumAggregate:
+          case WindowFunctionType::MinAggregate:
+          case WindowFunctionType::MaxAggregate:
+          case WindowFunctionType::Rank:
+            // Others starts at null.
+            values.emplace_back(ValueFactory::GetNullValueByType(TypeId::INTEGER));
+            break;
+        }
+      }
+      return {values};
+    }
+
+    /**
+     * TODO(Student)
+     *
+     * Combines the input into the aggregation result.
+     * @param[out] result The output aggregate value
+     * @param input The input value
+     */
+    void CombineAggregateValues(AggregateValue *result, const AggregateValue &input) {
+      for (uint32_t i = 0; i < agg_exprs_.size(); i++) {
+        switch (agg_types_[i]) {
+          case WindowFunctionType::CountStarAggregate:
+            result->aggregates_[i] = result->aggregates_[i].Add(input.aggregates_[i]);
+            break;
+          case WindowFunctionType::CountAggregate:
+            if (input.aggregates_[i].IsNull()) {
+              break;
+            }
+            if (result->aggregates_[i].IsNull()) {
+              result->aggregates_[i] = {TypeId::INTEGER, 1};
+            } else {
+              result->aggregates_[i] = result->aggregates_[i].Add({TypeId::INTEGER, 1});
+            }
+            break;
+          case WindowFunctionType::SumAggregate:
+            if (input.aggregates_[i].IsNull()) {
+              break;
+            }
+            if (result->aggregates_[i].IsNull()) {
+              result->aggregates_[i] = input.aggregates_[i];
+            } else {
+              result->aggregates_[i] = result->aggregates_[i].Add(input.aggregates_[i]);
+            }
+            break;
+          case WindowFunctionType::MinAggregate:
+            if (input.aggregates_[i].IsNull()) {
+              break;
+            }
+            if (result->aggregates_[i].IsNull()) {
+              result->aggregates_[i] = input.aggregates_[i];
+            } else {
+              result->aggregates_[i] = result->aggregates_[i].Min(input.aggregates_[i]);
+            }
+            break;
+          case WindowFunctionType::MaxAggregate:
+            if (input.aggregates_[i].IsNull()) {
+              break;
+            }
+            if (result->aggregates_[i].IsNull()) {
+              result->aggregates_[i] = input.aggregates_[i];
+            } else {
+              result->aggregates_[i] = result->aggregates_[i].Max(input.aggregates_[i]);
+            }
+            break;
+          case WindowFunctionType::Rank:
+            if (input.aggregates_[i].IsNull()) {
+              break;
+            }
+            if (result->aggregates_[i].IsNull()) {
+              result->aggregates_[i] = input.aggregates_[i];
+            } else {
+              result->aggregates_[i] = result->aggregates_[i].Add(input.aggregates_[i]);
+            }
+            break;
+        }
+      }
+    }
+
+    /**
+     * Inserts a value into the hash table and then combines it with the current aggregation.
+     * @param agg_key the key to be inserted
+     * @param agg_val the value to be inserted
+     */
+    void InsertCombine(const AggregateKey &agg_key, const AggregateValue &agg_val) {
+      if (ht_.count(agg_key) == 0) {
+        ht_.insert({agg_key, GenerateInitialAggregateValue()});
+      }
+      CombineAggregateValues(&ht_[agg_key], agg_val);
+    }
+
+    /**
+     * Clear the hash table
+     */
+    void Clear() { ht_.clear(); }
+
+    /** An iterator over the aggregation hash table */
+    class Iterator {
+     public:
+      /** Creates an iterator for the aggregate map. */
+      explicit Iterator(std::map<AggregateKey, AggregateValue>::const_iterator iter) : iter_{iter} {}
+
+      /** @return The key of the iterator */
+      auto Key() -> const AggregateKey & { return iter_->first; }
+
+      /** @return The value of the iterator */
+      auto Val() -> const AggregateValue & { return iter_->second; }
+
+      /** @return The iterator before it is incremented */
+      auto operator++() -> Iterator & {
+        ++iter_;
+        return *this;
+      }
+
+      /** @return `true` if both iterators are identical */
+      auto operator==(const Iterator &other) -> bool { return this->iter_ == other.iter_; }
+
+      /** @return `true` if both iterators are different */
+      auto operator!=(const Iterator &other) -> bool { return this->iter_ != other.iter_; }
+
+     private:
+      /** Aggregates map */
+      std::map<AggregateKey, AggregateValue>::const_iterator iter_;
+    };
+
+    /** @return Iterator to the start of the hash table */
+    auto Begin() -> Iterator { return Iterator{ht_.cbegin()}; }
+
+    /** @return Iterator to the end of the hash table */
+    auto End() -> Iterator { return Iterator{ht_.cend()}; }
+
+    auto IsEmpty() -> bool { return ht_.empty(); }
+
+    void Erase(const AggregateKey &key) { ht_.erase(key); }
+
+   private:
+    /** The hash table is just a map from aggregate keys to aggregate values */
+    std::map<AggregateKey, AggregateValue> ht_;
+    std::unordered_map<AggregateKey, uint64_t> cnt_table_;
+    /** The aggregate expressions that we have */
+    std::vector<AbstractExpressionRef> agg_exprs_;
+    /** The types of aggregations that we have */
+    std::vector<WindowFunctionType> agg_types_;
+  };
+
+  /** @return The tuple as an AggregateKey */
+  auto MakeAggregateKey(const Tuple *tuple, const std::vector<AbstractExpressionRef> &partition_by_) -> AggregateKey {
+    std::vector<Value> keys;
+    keys.reserve(partition_by_.size());
+    for (const auto &expr : partition_by_) {
+      keys.emplace_back(expr->Evaluate(tuple, child_executor_->GetOutputSchema()));
+    }
+    return {keys};
+  }
+
+  /** @return The tuple as an AggregateValue */
+  auto MakeAggregateValue(const Tuple *tuple, const AbstractExpressionRef &function) -> AggregateValue {
+    std::vector<Value> vals;
+    vals.emplace_back(function->Evaluate(tuple, child_executor_->GetOutputSchema()));
+    return {vals};
+  }
+
   /** The window aggregation plan node to be executed */
   const WindowFunctionPlanNode *plan_;
 
+  RID rid_{};
+  Tuple tuple_{};
   /** The child executor from which tuples are obtained */
   std::unique_ptr<AbstractExecutor> child_executor_;
+  std::vector<Tuple> origin_tuples_;
+  std::vector<std::vector<Tuple>> sort_tuples_;
+  std::vector<AggregationHashTable> ahts_;
+  // 对于非sort by语句，一次性聚合所有行，通过迭代器输出
+  std::vector<AggregationHashTable::Iterator> aht_iterators_;
+  uint64_t row_idx_{0};
+  // 记录每个key对应的窗口大小（元素个数）
+  std::vector<std::unordered_map<AggregateKey, uint64_t>> cnt_table_;
+  // 记录最远的sort或partition列索引 假设列值应该与最远sort顺序一致，若全无，否则应该与原始顺序一致
+  std::size_t sort_idx_{0};
+  // 以1为初值，避免移除下溢问题，更优雅地处理重复值
+  uint64_t cnt_duplication_{1};
 };
 }  // namespace bustub
